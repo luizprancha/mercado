59d4957223b272ddadd2ef3e04642fb0c215cd8f
aluno <aluno@10.61.7.36>
2026-05-07 11:53:58 -0300

    commitzinho

--- src/view/TelaLogin.java
+++ src/view/TelaLogin.java
@@ -20,7 +20,7 @@ public class TelaLogin extends JPanel {
     private JLabel lblNewLabel_1;
 
     public TelaLogin() {
-        setBackground(new Color(244, 247, 246));
+        setBackground(new Color(245, 235, 220));
         setToolTipText("Tela Login");
 
         setLayout(new MigLayout("", "[grow][grow][][grow][grow]", "[][grow][grow][grow][grow][grow][grow][grow][grow]"));
@@ -30,31 +30,39 @@ public class TelaLogin extends JPanel {
         add(lblNewLabel_1, "cell 1 1 3 1,alignx center");
         
         lblNewLabel = new JLabel("Por favor, acesse sua conta ou crie uma nova.");
-        lblNewLabel.setFont(new Font("Times New Roman", Font.PLAIN, 14));
+        lblNewLabel.setFont(new Font("Alef", Font.PLAIN, 14));
         add(lblNewLabel, "cell 1 2 3 1,alignx center");
 
         JLabel lblNome = new JLabel("Nome:");
-        lblNome.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+        lblNome.setFont(new Font("Alef", Font.BOLD, 20));
         add(lblNome, "cell 1 4,alignx right");
 
         tfNome = new JTextField();
+        tfNome.setFont(new Font("Alef", Font.PLAIN, 14));
         add(tfNome, "cell 3 4,growx");
         tfNome.setColumns(10);
 
         JLabel lblCpf = new JLabel("CPF:");
-        lblCpf.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+        lblCpf.setFont(new Font("Alef", Font.BOLD, 20));
         add(lblCpf, "cell 1 5,alignx right");
 
         tfCpf = new JTextField();
+        tfCpf.setFont(new Font("Alef", Font.PLAIN, 14));
         add(tfCpf, "cell 3 5,growx");
         tfCpf.setColumns(10);
 
         btEntrar = new JButton("Entrar");
-        btEntrar.setFont(new Font("Times New Roman", Font.PLAIN, 11));
+        btEntrar.setForeground(new Color(255, 255, 255));
+        btEntrar.setFont(new Font("Alef", Font.BOLD, 20));
+        btEntrar.setBackground(new Color(8, 69, 145));
+        btEntrar.setBorderPainted(false);
         add(btEntrar, "cell 1 7,alignx center");
 
         btCadastrar = new JButton("Cadastrar");
-        btCadastrar.setFont(new Font("Times New Roman", Font.PLAIN, 11));
+        btCadastrar.setForeground(new Color(255, 255, 255));
+        btCadastrar.setFont(new Font("Alef", Font.BOLD, 20));
+        btCadastrar.setBackground(new Color(8, 69, 145));
+        btCadastrar.setBorderPainted(false);
         add(btCadastrar, "cell 3 7,alignx center");
     }
 
